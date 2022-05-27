326f390a934f38cb4d1b6bdc3fa2f8e017037e07
vinci shen <shenvinci@gmail.com>
2022-05-27 13:56:11 +0800

    实现GUI功能

--- Robber.py
+++ Robber.py
@@ -1,21 +1,33 @@
 from selenium.webdriver import Edge
-from selenium.webdriver.edge.options import Options as EdgeOptions
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.edge.service import Service
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+
 
 class Robber:
-    def __init__(self, User, Pwd, year, classKey) -> None:
+    def __init__(self, User: str, Pwd: str, year: str, classKey, date=None):
+        """初始化
+
+        Args:
+            User (str): 用户名
+            Pwd (str): 用户密码
+            year (str): 选择的学年，如'2021-2022-2'
+            classKey (_type_): 课程编号，可以是列表或者单个编号
+            date (str): 选择的具体时间
+        """
         self.User = User
         self.Pwd = Pwd
         self.year = year
         self.classKey = classKey
+        self.date = date
 
     def initBrower(self, debug: bool = False):
         """对网页进行初始化"""
-        options = EdgeOptions()
+        options = Options()
         options.add_experimental_option(
             "excludeSwitches", ['disable-automation', 'enable-logging'])
         if not debug:
@@ -40,14 +52,37 @@ class Robber:
         self.browser.find_element(
             By.XPATH, "/html/body/div/div/center/table[2]/tbody/tr[8]/td[2]/a").click()
 
-    def searchClass(self):
+    def robCourse(self):
+        """抢课函数"""
+        if self.date:
+            self._robCourseByTime()
+        else:
+            self._justRobCourse()
+
+    def _robCourseByTime(self):
+        """使用定时器进行抢课"""
+        sched = BlockingScheduler(timezone='Asia/Shanghai')
+        sched.add_job(self._justRobCourse, trigger='date', run_date=self.date)
+        sched.start()
+
+    def _justRobCourse(self):
+        """进行普通的抢课"""
+        if type(self.classKey) == list:
+            for key in self.classKey:
+                self._searchClass(key)
+                self._selectCourse()
+        elif type(self.classKey) == str:
+            self._searchClass(self.classKey)
+            self._selectCourse()
+
+    def _searchClass(self, key):
         """搜索相应编号的课程"""
         self.browser.find_element(
             By.XPATH, "/html/body/div/center/table[1]/tbody/tr[2]/td[2]/select/option[2]").click()
         self.browser.find_element(By.NAME, 'ww').send_keys(
-            self.classKey, Keys.ENTER)
+            key, Keys.ENTER)
 
-    def selectCourse(self):
+    def _selectCourse(self):
         """选择课程"""
         self.browser.find_element(
             By.XPATH, '/html/body/div/center/table[3]/tbody/tr[2]/td[28]/a').click()
---
+++ windows.py
@@ -0,0 +1,45 @@
+from PySide6.QtWidgets import *
+from PySide6.QtCore import QFile
+from PySide6.QtUiTools import QUiLoader
+
+from Robber import Robber
+from qt_material import apply_stylesheet
+
+class Window:
+    def __init__(self):
+        super(Window, self).__init__()
+
+        # 加载ui文件
+        qfile = QFile("windows.ui")
+        qfile.open(QFile.ReadOnly)
+        qfile.close()
+
+        # 创建ui窗口对象
+        self.ui = QUiLoader().load(qfile)
+        self.ui.pushButton.clicked.connect(self.btnClick)
+
+    def btnClick(self):
+        user = self.ui.User.toPlainText()   # 获取文本信息
+        pwd = self.ui.Pwd.toPlainText()
+        year = self.ui.year.currentText()
+        classKey = self.ui.classKey.toPlainText()
+        rob(user, pwd, year, classKey)
+
+
+def rob(User, Pwd, year, classKey, date=None):
+    """抢课函数"""
+    robber = Robber(User, Pwd, year, classKey, date)
+    robber.initBrower(True)
+    robber.login()
+    robber.loadTable()
+    robber.robCourse()
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    # app.setWindowIcon(QIcon("logo.png"))    # 添加图标
+    w = Window()
+    QApplication.setStyle(QStyleFactory.create('Fusion'))
+    apply_stylesheet(app, theme='dark_red.xml')
+    w.ui.show()
+    app.exec()
